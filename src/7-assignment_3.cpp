a0f25319f85881f6fbcd960690e4cc48f174a6bc
Soraia Meneses Alarcão <lua.svmac@gmail.com>
2014-02-22 12:52:43 +0000

    alterei o opengl version para 3.1 :P

--- src/7-assignment_3.cpp
+++ src/7-assignment_3.cpp
@@ -398,7 +398,7 @@ void setupGLUT(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	
-	glutInitContextVersion(3, 3);
+	glutInitContextVersion(3, 1);
 	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
 

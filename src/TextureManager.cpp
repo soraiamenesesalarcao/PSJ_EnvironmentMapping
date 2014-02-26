7bc00c42bad857f6e332b9a2a34513ba061ba5c2
catiadias <catiadias089@gmail.com>
2014-02-26 00:26:58 +0000

    Classe TextureManager

---
+++ src/TextureManager.cpp
@@ -0,0 +1,82 @@
+#include "TextureManager.h"
+#include "SOIL.h"
+
+const GLuint TextureManager::RED = 1;
+const GLuint TextureManager::GREEN = 2;
+
+TextureManager* TextureManager::_inst(0);
+
+TextureManager* TextureManager::Inst()
+{
+	if(!_inst)
+		_inst = new TextureManager();
+
+	return _inst;
+}
+
+TextureManager::TextureManager(){
+	_texID = std::vector<GLuint>(13);
+	glGenTextures(2, &_texID[0]); 
+	loadTexture("../scripts/textures/stone.tga", RED);
+	//loadTexture("../src/objs/green.png", GREEN); 
+}
+
+void TextureManager::loadTexture(const char* dirName, int textID){
+	int width, height;
+	int channel;
+	unsigned char* image = SOIL_load_image(dirName, &width, &height, &channel, SOIL_LOAD_RGB);
+	glBindTexture(GL_TEXTURE_2D, _texID[textID]);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,GL_UNSIGNED_BYTE, image);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); // Linear Filtering (optional)
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); // Linear Filtering (optional)
+	SOIL_free_image_data(image);
+}
+
+
+//these should never be called
+//TextureManager::TextureManager(const TextureManager& tm){}
+//TextureManager& TextureManager::operator=(const TextureManager& tm){}
+	
+TextureManager::~TextureManager(){
+	UnloadAllTextures();
+	_inst = 0;
+}
+
+bool TextureManager::LoadTexture(const char* filename, const unsigned int texID, GLenum image_format, GLint internal_format, GLint level, GLint border) {
+	//Not implemented
+	return false;
+}
+
+bool TextureManager::UnloadTexture(const unsigned int texID){
+	bool result(true);
+	//if this texture ID mapped, unload it's texture, and remove it from the vector
+	if(texID < _texID.size()){
+		glDeleteTextures(1, &(_texID[texID]));
+		_texID.erase(_texID.begin()+texID);
+	}
+	//otherwise, unload failed
+	else
+	{
+		result = false;
+	}
+
+	return result;
+}
+
+bool TextureManager::BindTexture(const unsigned int texID)
+{
+	bool result(true);
+	//if this texture ID mapped, bind it's texture as current
+	if(texID < _texID.size())
+		glBindTexture(GL_TEXTURE_2D, _texID[texID]);
+	//otherwise, binding failed
+	else
+		result = false;
+
+	return result;
+}
+
+void TextureManager::UnloadAllTextures(){
+	while(!_texID.empty())
+		UnloadTexture(0);
+}
---
+++ src/TextureManager.h
@@ -0,0 +1,47 @@
+#ifndef TextureManager_H
+#define TextureManager_H
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <vector>
+#include <iostream>
+
+class TextureManager
+{
+public:
+
+
+	static const GLuint RED;
+	static const GLuint GREEN;
+
+	static TextureManager* Inst();
+	virtual ~TextureManager();
+
+	bool LoadTexture(const char* filename,	//where to load the file from
+		const unsigned int texID,			//arbitrary id you will reference the texture by
+											//does not have to be generated with glGenTextures
+		GLenum image_format = GL_RGB,		//format the image is in
+		GLint internal_format = GL_RGB,		//format to store the image in
+		GLint level = 0,					//mipmapping level
+		GLint border = 0);					//border size
+
+	//free the memory for a texture
+	bool UnloadTexture(const unsigned int texID);
+
+	//set the current texture
+	bool BindTexture(const unsigned int texID);
+
+	//free all texture memory
+	void UnloadAllTextures();
+
+protected:
+	TextureManager();
+	TextureManager(const TextureManager& tm);
+	TextureManager& operator=(const TextureManager& tm);
+
+	static TextureManager* _inst;
+	std::vector<GLuint> _texID;
+	void loadTexture(const char* dirName, int textID);
+};
+
+#endif

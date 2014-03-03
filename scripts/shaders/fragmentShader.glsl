6cf6e1716a786119f55d00ea13018250bbf10b0d
catiadias <catiadias089@gmail.com>
2014-03-03 11:42:10 +0000

    Shaders - texturas

--- scripts/shaders/fragmentShader.glsl
+++ scripts/shaders/fragmentShader.glsl
@@ -1,70 +1,17 @@
 #version 330 core
 
-// In
-in vec4 out_Color
-in vec3 out_Normal
-in vec3 out_Position
-in vec2 out_Texcoord;
+// Interpolated values from the vertex shaders
+in vec2 UV;
+in vec3 Position_worldspace;
 
-// Out
-out vec4 out_Color_Final;
+// Ouput data
+out vec3 color;
 
-// Light Attributes
-uniform vec3 light_Position;
-uniform vec3 light_Ambient;
-uniform vec3 light_Diffuse;
-uniform vec3 light_Specular;
-uniform float light_Distance;
-uniform float light_ConstantAttenuation; 
-uniform float light_LinearAttenuation;
-uniform float light_QuadraticAttenuation;
+// Values that stay constant for the whole mesh.
+uniform sampler2D Texture;
 
+void main(){
+	vec3 MaterialDiffuseColor = texture( Texture, UV ).rgb;
+	color =	MaterialDiffuseColor;
 
-// Material Attributes
-uniform vec3 material_Ambient;
-uniform vec3 material_Diffuse;
-uniform vec3 material_Specular;
-uniform float material_Shininess;
-
-// Camera View Vector
-uniform vec3 EyeDirection;
-
-// Matrices
-uniform mat4 mtx_Model;
-layout(std140) uniform Matrices{
-	mat4 mtx_View;
-	mat4 mtx_Projection;
-};
-
-void main(void) {
-
-	// Phong Shading + Blinn-Phong Illumination + Depth Cueing
-	vec3 L = normalize(vec3(mtx_View * vec4(light_Position, 1.0)) - ex_Position); //sem view -> luz segue camara, com view -> luz segue cena
-	vec3 N = normalize(out_Normal);
-	//vec3 V = normalize(-EyeDirection);
-	vec3 V = normalize(-LightPosition);
-	vec3 H = normalize(L + V);
-
-	// Ambient Component
-	vec3 comp_Ambient = material_Ambient * light_Ambient;
-
-	// Diffuse Component	
-	float NdotL = max(dot(N, L), 0.0);
-	vec3 comp_Diffuse = material_Diffuse * light_Diffuse * NdotL;
-
-	// Specular Component (Blinn-Phong)
-	vec3 specular = vec3(0.0);
-	if(NdotL > 0) {
-		float NdotH = max(dot(N, H), 0.0);
-		specular = material_Specular * light_Specular * pow(NdotH, material_Shininess);
-	}
-
-	// Depth Cueing	
-	depth_Cueing = light_ConstantAttenuation + 
-				   light_LinearAttenuation * light_Distance +
-				   light_QuadraticAttenuation * pow(light_Distance, 2);
-
-	// Final
-	vec4 lightIntensity = vec4( comp_Ambient + (comp_Diffuse + comp_Specular)/depth_Cueing, 1.0);
-	out_Color = LightIntensity
 }
--- scripts/shaders/vertexShader.glsl
+++ scripts/shaders/vertexShader.glsl
@@ -1,29 +1,25 @@
 #version 330 core
 
-// In
-in vec4 in_Color
-in vec3 in_Normal
-in vec3 in_Position
-in vec2 in_Texcoord;
+// Input vertex data, different for all executions of this shader.
+layout(location = 0) in vec4 in_Position;
+layout(location = 1) in vec4 in_Normal;
+layout(location = 2) in vec2 in_UV;
 
-// Out
-out vec4 out_Color
-out vec3 out_Normal
-out vec3 out_Position
-out vec2 out_Texcoord;
+// Output data ; will be interpolated for each fragment.
+out vec2 UV;
 
-// Matrices
-uniform mat4 mtx_Model;
-uniform mat3 mtx_Normal;
-layout(std140) uniform Matrices {
-	mat4 mtx_Projection;
-	mat4 mtx_View;
+uniform vec4 Color;
+uniform mat4 ModelMatrix;
+layout(std140) uniform SharedMatrices
+{
+	mat4 ViewMatrix;
+	mat4 ProjectionMatrix;
 };
 
-void main(void){
-	out_Color = in_Color;
-	out_Normal = normalize(mtx_Normal * in_Normal);
-	out_Position = vec3(mtx_View * mtx_Model * vec4(in_Position, 1.0));
-	out_Texcoord = in_Texcoord;
-	gl_Position = mtx_Projection * mtx_View * mtx_Model * vec4(in_Position,1.0);
+void main(){
+	// Output position of the vertex, in clip space : ProjectionMatrix * ViewMatrix * ModelMatrix * position
+	gl_Position =  ProjectionMatrix * ViewMatrix * ModelMatrix * in_Position;
+	
+	// UV of the vertex. No special space for this one.
+	UV = in_UV;
 }
